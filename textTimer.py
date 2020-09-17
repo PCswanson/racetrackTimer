efbe5e1e1d1de47cf464caac5a31d8ef203652c1
PCswanson <kris.swanson@pinecrest.edu>
2020-09-17 12:09:27 -0400

    text timer 1

---
+++ textTimer.py
@@ -0,0 +1,53 @@
+#TextTimer
+import time
+from time import sleep
+from datetime import datetime
+from gpiozero import Button
+
+starter_switch = Button(2)
+t1Gate = Button(3)
+t2Gate = Button(4)
+
+run_number = 1
+t1 = 0
+t2 = 0
+t1State = 0
+t2State = 0
+
+timer = 0
+
+
+
+while True:
+
+#display dateTime, run number
+    print(datetime.now())
+    print("Run #", run_number)
+
+
+#wait for switch press
+    starter_switch.wait_for_press()
+    print("Ready for Race!")
+#wait for switch unpress, start timer
+
+    starter_switch.wait_for_release()
+    timer = time.perf_counter()
+    print("Race Begins!")
+
+    while t1State == 0 or t2State == 0:
+        if t1Gate.is_pressed:
+            t1State = 1
+            t1 = round(time.perf_counter() - timer,3)
+        if t2Gate.is_pressed:
+            t2State = 1
+            t2 = round(time.perf_counter() - timer, 3)
+
+#check for finish, when each finishes, assign time to track
+
+#display time, run number, date time, save to file, return to top
+
+    print("Track 1: ", t1)
+    print("Track 2: ", t2)
+    run_number = run_number + 1
+    t1State = 0
+    t2State = 0
